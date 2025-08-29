66551e6bb780100d47395d3a282543fea89fedd7
khzondojhb024 <khulekaniszondo6@gmail.com>
2025-08-29 22:52:42 +0200

    added project logos

--- display.js
+++ display.js
@@ -1,53 +1,53 @@
-let visibility = false;
+// let visibility = false;
 
-function showProjects(){
-    const projectdiv = document.getElementById("personalprojects");
+// function showProjects(){
+//     const projectdiv = document.getElementById("personalprojects");
     
 
-    if (!visibility){
-        projectdiv.style.display = "block";
-        projectdiv.style.border = "2px solid yellow";
-        visibility = true;
+//     if (!visibility){
+//         projectdiv.style.display = "block";
+//         projectdiv.style.border = "2px solid yellow";
+//         visibility = true;
         
-    }else{
-        if (visibility){
-          // formdiv.style.border = "2px solid yellow";
-          projectdiv.style.display = "none";
-          projectdiv.style.display = "none";
+//     }else{
+//         if (visibility){
+//           // formdiv.style.border = "2px solid yellow";
+//           projectdiv.style.display = "none";
+//           projectdiv.style.display = "none";
 
-          formdiv.style.height = "15%";
-        }
+//           formdiv.style.height = "15%";
+//         }
 
-       visibility = false; 
-      }
+//        visibility = false; 
+//       }
     
-}
+// }
 
-function toggle(){
+// function toggle(){
       
-      const formdiv = document.getElementById("personalprojects");
+//       const formdiv = document.getElementById("personalprojects");
     
 
    
-      if (!visibility){
-        routeform.style.display = "block";
-        formdiv.style.display = "block";
-        formdiv.style.border = "2px solid blue";
+//       if (!visibility){
+//         routeform.style.display = "block";
+//         formdiv.style.display = "block";
+//         formdiv.style.border = "2px solid blue";
         
-        exitbutton.style.display = "block";
-        formdiv.style.height = "55%";
-        formdiv.style.borderRadius ="15px";
-        visibility = true;
+//         exitbutton.style.display = "block";
+//         formdiv.style.height = "55%";
+//         formdiv.style.borderRadius ="15px";
+//         visibility = true;
         
-      }else{
-        if (visibility){
-          // formdiv.style.border = "2px solid yellow";
-          routeform.style.display = "none";
-          exitbutton.style.display = "none";
+//       }else{
+//         if (visibility){
+//           // formdiv.style.border = "2px solid yellow";
+//           routeform.style.display = "none";
+//           exitbutton.style.display = "none";
 
-          formdiv.style.height = "15%";
-        }
+//           formdiv.style.height = "15%";
+//         }
 
-       visibility = false; 
-      }
-    }
+//        visibility = false; 
+//       }
+//     }
